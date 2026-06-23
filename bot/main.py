45507335b71e9028cffd44ed9bfea9972167a796
kenny1102bleu-lab <kenny1102bleu@gmail.com>
2026-06-23 12:58:44 +0900

    fix: use Render PORT env var instead of hardcoded 8080

--- bot/main.py
+++ bot/main.py
@@ -6,5 +6,5 @@ from bot.webhook_server import start as start_webhook, set_register_callback
 import bot.discord_bot as bot_module
 
 set_register_callback(bot_module.register_pending)
-start_webhook(port=8080)
+start_webhook()
 bot_module.bot.run(bot_module.TOKEN)
